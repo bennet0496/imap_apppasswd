697bb311f285bb4a29095483cb13ca5c395c4f6f
Bennet Becker <bbecker@pks.mpg.de>
2024-01-25 09:49:11 +0100

    Initial Version

---
+++ imap_apppasswd.js
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2024. Bennet Becker <dev@bennet.cc>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+function apppw_remove(id) {
+    rcmail.http_post("plugin.imap_apppasswd_remove", {'id': id});
+    // document.querySelector('[data-apppw-id="' + id +'"]').remove();
+}
+
+function apppw_add() {
+    rcmail.http_post("plugin.imap_apppasswd_add");
+}
+
+function delete_all() {
+    rcmail.http_post("plugin.imap_apppasswd_delete_all");
+}
+
+function apppw_edit(id) {
+    let elm = document.querySelector('[data-apppw-id="' + id +'"] .apppw_title .apppw_title_text');
+    let btn = document.querySelector('[data-apppw-id="' + id +'"] .apppw_title .apppw_title_edit');
+
+    let box = document.createElement("input");
+    box.value = elm.innerText;
+    box.id = "apppw_title_box_" + id;
+    box.className = "apppw_title_box";
+    box.type = "text";
+    elm.replaceWith(box);
+    box.focus();
+
+    btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>';
+    btn.title = window.rcmail.gettext("done", "imap_apppasswd")
+    btn.onclick = function () {
+        rcmail.http_post("plugin.imap_apppasswd_rename", {"id": id, "name": box.value});
+    }
+}
+
+rcmail.addEventListener("plugin.apppw_remove_from_list", function (data){
+    // alert();
+    document.querySelector('[data-apppw-id="' + data.id +'"]').remove();
+}, true);
+
+rcmail.addEventListener("plugin.apppw_add", function (data) {
+    let node = document.createElement("div")
+    node.className = "apppw_entry";
+    node.dataset.apppwId = data.id;
+
+    let title = document.createElement("span")
+    title.className = "apppw_title";
+    // title.innerText = "Unnamed";
+
+    let title_box = document.createElement("input");
+    title_box.value = window.rcmail.gettext("unnamed_app", "imap_apppasswd");
+    title_box.id = "apppw_title_box_" + data.id;
+    title_box.className = "apppw_title_box";
+    title_box.type = "text";
+
+    let title_btn = document.createElement("a");
+    title_btn.className = "apppw_title_edit";
+    title_btn.title = window.rcmail.gettext("done", "imap_apppasswd");
+    title_btn.onclick = function () {
+        rcmail.http_post("plugin.imap_apppasswd_rename", {"id": data.id, "name": title_box.value});
+    };
+    title_btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>';
+
+    let content = document.createElement("span")
+    content.className = "apppw_content";
+
+    let box = document.createElement("input");
+    box.value = data.passwd;
+    box.id = "apppw_content_box_" + data.id;
+    box.type = "password";
+    // box.readOnly = true;
+    box.onkeydown = function (ev) {
+        return false;
+    }
+
+
+    let copy = document.createElement("span");
+    copy.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>'
+    copy.className = "apppw_content_action";
+    copy.onclick = function () {
+        navigator.clipboard.writeText(data.passwd).then(() => {
+            window.rcmail.display_message(window.rcmail.gettext("copied", "imap_apppasswd"));
+        }).catch(() => {
+            window.rcmail.display_message(window.rcmail.gettext("copy_failed_check_perms", "imap_apppasswd"), "error");
+        });
+    }
+    let show = document.createElement("span");
+    show.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>';
+    show.className = "apppw_content_action";
+    show.onclick = function (ev) {
+        let box = document.getElementById("apppw_content_box_" + data.id);
+        let target = ev.target.nodeName === 'svg' ? ev.target.parentNode : ev.target;
+
+        if(box.type === "password") { //show
+            box.type = "text";
+            target.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/></svg>'
+        } else if (box.type === "text") {
+            box.type = "password";
+            target.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>';
+        }
+    }
+
+    let ok = document.createElement("a");
+    ok.className = "apppw_delete";
+    ok.innerText = rcmail.gettext("ok", "imap_apppasswd");
+    ok.onclick = function (ev) {
+        ev.target.innerText = rcmail.gettext("delete", "imap_apppasswd");
+        document.querySelector("[data-apppw-id=\"" + data.id +"\"] .apppw_content").remove();
+        ev.target.onclick = () => apppw_remove(data.id);
+        //rename
+        rcmail.http_post("plugin.imap_apppasswd_rename", {"id": data.id, "name": title_box.value});
+    }
+
+    content.append(box);
+    content.append(show);
+    content.append(copy);
+    title.append(title_box);
+    title.append(title_btn)
+    node.append(title);
+    node.append(content);
+    node.append(ok);
+
+    document.getElementById("apppw_list").append(node);
+});
+
+rcmail.addEventListener("plugin.apppw_renamed", function(data) {
+    let box = document.querySelector('[data-apppw-id="' + data.id +'"] .apppw_title .apppw_title_box');
+    let btn = document.querySelector('[data-apppw-id="' + data.id +'"] .apppw_title .apppw_title_edit');
+
+    let span = document.createElement("span");
+    span.className = "apppw_title_text";
+    span.innerText = data.name;
+
+    box.replaceWith(span);
+    btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>';
+    btn.onclick = () => apppw_edit(data.id);
+});
---
+++ imap_apppasswd.php
@@ -0,0 +1,243 @@
+<?php
+// Copyright (c) 2024 Bennet Becker <dev@bennet.cc>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//namespace bennetcc;
+//
+//class_alias('\bennetcc\imap_apppasswd', '\imap_apppasswd');
+
+class imap_apppasswd extends \rcube_plugin
+{
+    public $task = 'settings';
+    private string $log_table = 'userlogins';
+    private \rcmail $rc;
+    private \PDO $db;
+
+    function init(): void
+    {
+        $this->load_config('config.inc.php.dist');
+        $this->load_config();
+        $this->add_texts('l10n/', true);
+        $this->rc = \rcmail::get_instance();
+
+        if ($this->rc->task == 'settings') {
+            $this->add_hook('settings_actions', [$this, 'settings_actions']);
+            $this->register_action('plugin.imap_apppasswd', [$this, 'show_settings']);
+            $this->register_action('plugin.imap_apppasswd_remove', [$this, 'remove_password']);
+            $this->register_action('plugin.imap_apppasswd_delete_all', [$this, 'delete_all']);
+            $this->register_action('plugin.imap_apppasswd_add', [$this, 'add_password']);
+            $this->register_action('plugin.imap_apppasswd_rename', [$this, 'rename_password']);
+
+            $this->db = new PDO("mysql:host=db.example.com;dbname=mail", "roundcube", "Eemeep2cheil4dee5ahShohquo5EC6ko"); //\mysqli("db.example.com", "roundcube", "Eemeep2cheil4dee5ahShohquo5EC6ko", "mail");
+        }
+    }
+
+    /**
+     * Add a tab to Settings.
+     */
+    public function settings_actions($args): array
+    {
+        $args['actions'][] = [
+            'action' => 'plugin.imap_apppasswd',
+            'class'  => 'imap_apppasswd',
+            'label'  => 'imap_apppasswd',
+            'domain' => 'imap_apppasswd',
+        ];
+
+        return $args;
+    }
+
+    public function show_settings(): void
+    {
+        $this->register_handler('plugin.body', [$this, 'settingshtml']);
+        $this->rc->output->set_pagetitle($this->gettext('imap_apppasswd'));
+        $this->rc->output->send('plugin');
+    }
+
+    /**
+     * Settings tab content.
+     */
+    public function settingshtml(): string
+    {
+        $this->include_stylesheet("imap_apppasswd.css");
+        $this->include_script("imap_apppasswd.js");
+
+        $s = $this->db->prepare("SELECT pws.*, pwid, src_ip, src_rdns, src_loc, src_isp, timestamp as last_used
+            FROM (SELECT * FROM app_passwords WHERE uid = :uid) pws
+            LEFT JOIN (WITH s1 AS (
+                SELECT *, RANK() OVER (PARTITION BY log.pwid ORDER BY log.timestamp DESC) AS `Rank` FROM log
+            ) SELECT * FROM s1 WHERE `Rank` = 1 ORDER BY timestamp) l ON l.pwid = pws.id ORDER BY created;");
+        $user_name = explode("@",$this->rc->get_user_name())[0];
+//        \rcmail::write_log('imap_apppw', "username ".$this->rc->get_user_name()." ".$this->rc->get_user_id());
+        $s->bindValue("uid", $user_name);
+        $s->execute();
+//        $r = $s->get_result();
+
+        $table = new \html_table([]);
+        $table->add_row();
+        $table->add_header([], "");
+        $table->add_header([], $this->gettext("imap_setting"));
+        $table->add_header([], $this->gettext("smtp_setting"));
+        $table->add_row();
+
+        $table->add([], $this->gettext("server"));
+        $table->add([], $this->rc->config->get("advertised_client_imap_host"));
+        $table->add([], $this->rc->config->get("advertised_client_smtp_host"));
+        $table->add_row();
+        $table->add([], $this->gettext("port"));
+        $table->add([], $this->rc->config->get("advertised_client_imap_port"));
+        $table->add([], $this->rc->config->get("advertised_client_smtp_port"));
+        $table->add_row();
+        $table->add([], $this->gettext("protocol"));
+        $table->add([], $this->rc->config->get("advertised_client_imap_protocol"));
+        $table->add([], $this->rc->config->get("advertised_client_smtp_protocol"));
+        $table->add_row();
+        $table->add([], $this->gettext("password_method"));
+        $table->add([], $this->rc->config->get("advertised_client_imap_password_method"));
+        $table->add([], $this->rc->config->get("advertised_client_smtp_password_method"));
+
+        $html = "";
+
+        while ($row = $s->fetch(\PDO::FETCH_ASSOC)) {
+            $now = new DateTimeImmutable();
+            $lu = new DateTimeImmutable($row['last_used']);
+            $ct = new DateTimeImmutable($row['created']);
+
+            $now->diff($lu)->format("%d days ago");
+
+            $html .= \html::div(['class' => 'apppw_entry', 'data-apppw-id' => $row['id']],
+                \html::span(['class' => 'apppw_title'],
+                    \html::span(['class' => 'apppw_title_text'], ($row['comment'] ?? $this->gettext('unnamed_app'))).
+                            \html::a(['class' => 'apppw_title_edit', 'title' => $this->gettext('edit'), 'onclick' => 'apppw_edit('.$row['id'].')'], '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>')).
+                        \html::span(['class' => 'apppw_lastused', 'title' => $row['last_used'] == null ? $this->gettext('never_used') : $lu->format(DATE_RFC822)],
+                            $row['last_used'] == null ?
+                            $this->gettext('never_used') :
+                            $this->gettext('last_used')." ".$this->format_diff($now->diff($lu))." ".$this->gettext('last_used_from')." ".
+                                \html::span(['title' => $row['src_ip']],
+                                    (empty($row['src_rdns']) ? $row['src_ip'] : $row['src_rdns']).(empty($row['src_isp']) ? "" : " (".$row['src_isp'].")"))).
+                        (empty($row['src_loc']) ? "" : \html::span(['class' => 'apppw_location'], $row['src_loc'])).
+                        \html::span(['class' => 'apppw_created', 'title' => $ct->format(DATE_RFC822)], $this->gettext('created')." ".$this->format_diff($now->diff($ct))).
+                        \html::a(['class' => 'apppw_delete', 'onclick' => 'apppw_remove('.$row['id'].')'], $this->gettext("delete"))
+            );
+        }
+
+//        $this->inc
+        return \html::div(["class" => "apppw_info"],
+            \html::p([], $this->gettext("page_allows_password_creation_for_clients")).
+            \html::p([], $this->gettext("use_the_following_settings")). $table->show()
+        ).\html::div(['class' => 'apppw','id' => 'apppw_container'], \html::div(['class' => 'apppw_list','id' => 'apppw_list'],$html).
+            \html::div(['class' => 'apppw_actions'],
+                (new \html_button(['onclick' => 'apppw_add()', 'class' => 'create']))->show($this->gettext('add')).
+                (new \html_button(['onclick' => 'apppw_remove_all()', 'class' => 'delete']))->show($this->gettext('delete_all'))
+            ));
+    }
+
+    public function remove_password(mixed $attr = null) : void
+    {
+        rcmail::write_log('imap_apppw', print_r($attr, true));
+        rcmail::write_log('imap_apppw', print_r($_REQUEST, true));
+
+        $s = $this->db->prepare("DELETE FROM app_passwords WHERE id = :id;");
+//        $s->bind_param("i", $_REQUEST['id']);
+        $s->bindValue("id", $_REQUEST['id'], \PDO::PARAM_INT);
+
+        if($s->execute()) {
+            $this->rc->output->command("plugin.apppw_remove_from_list", ["id" => $_REQUEST['id']]);
+            $this->rc->output->show_message($this->gettext("apppw_deleted_success"));
+        } else {
+            $this->rc->output->show_message($this->gettext("apppw_deleted_error"), "error");
+        }
+    }
+
+    public function add_password() : void
+    {
+        try {
+            $rand = random_bytes($this->rc->config->get('imap_apppasswd_length', 16));
+            $salt = base64_encode(random_bytes(16));
+        } catch (\Random\RandomException $e) {
+            $rand = openssl_random_pseudo_bytes($this->rc->config->get('imap_apppasswd_length', 16));
+            $salt = openssl_random_pseudo_bytes(16);
+        }
+
+        $pw = implode("-", str_split(implode(array_map(function($c) {
+            $i = ord($c);
+            $i = $i % (26 + 26 + 10);
+
+            if($i < 10) {
+                return chr($i + 48);
+            } else if ($i < 36) {
+                return chr($i - 10 + 65);
+            } else {
+                return chr($i - 10 - 26 + 97);
+            }
+        }, str_split($rand))), $this->rc->config->get('imap_apppasswd_chunksize', 4)));
+
+        $hash = "{CRYPT}".crypt($pw, "$6$".$salt);
+        rcmail::write_log('imap_apppw', $hash);
+
+
+        $s = $this->db->prepare("INSERT INTO app_passwords (uid, password) VALUES (:uid, :password);");
+//        $s->bind_param("ss", explode("@", $this->rc->get_user_name())[0], $hash);
+        $s->bindValue("uid", explode("@", $this->rc->get_user_name())[0]);
+        $s->bindValue("password", $hash);
+
+        if($s->execute()) {
+            $this->rc->output->command("plugin.apppw_add", ["id" => $this->db->lastInsertId(),"passwd" => $pw]);
+        } else {
+            $this->rc->output->show_message($this->gettext("apppw_add_error"), "error");
+        }
+
+    }
+
+    public function rename_password() : void
+    {
+        $name = strip_tags($_REQUEST['name']);
+        $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT);
+        $s = $this->db->prepare("UPDATE app_passwords SET comment = :comment WHERE id = :id;");
+        $s->bindValue("comment", $name, \PDO::PARAM_STR);
+        $s->bindValue("id", $id, \PDO::PARAM_INT);
+
+        if($s->execute()) {
+            $this->rc->output->command("plugin.apppw_renamed", ["id" => $id, "name" => $name]);
+        } else {
+            $this->rc->output->show_message($this->gettext("apppw_rename_error"), "error");
+        }
+    }
+    private function format_diff(DateInterval $diff) : string {
+        if($diff->format("%y") != "0") {
+            return sprintf($this->gettext("years_ago"), $diff->format("%y"));
+        }
+        if($diff->format("%m") != "0") {
+            return sprintf($this->gettext("months_ago"), $diff->format("%m"));
+        }
+        if($diff->format("%d") != "0") {
+            return sprintf($this->gettext("days_ago"), $diff->format("%d"));
+        }
+        if($diff->format("%h") != "0") {
+            return sprintf($this->gettext("hours_ago"), $diff->format("%h"));
+        }
+        if($diff->format("%i") != "0") {
+            return sprintf($this->gettext("minutes_ago"), $diff->format("%i"));
+        }
+
+        return $this->gettext("just_now");
+
+    }
+}
